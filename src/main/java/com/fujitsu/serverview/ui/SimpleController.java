ee3c0a9b5c28d5098a1618419a24816c150f961c
Maciej Solinski <Maciej.Solinski@ts.fujitsu.com>
2017-08-04 16:08:08 +0200

    Initial commit

---
+++ src/main/java/com/fujitsu/serverview/ui/SimpleController.java
@@ -0,0 +1,33 @@
+package com.fujitsu.serverview.ui;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Maciej Solinski <a href="mailto:maciej.solinski@ts.fujitsu.com">maciej.solinski@ts.fujitsu.com</a>
+ */
+@RestController
+@RequestMapping("/simple")
+public class SimpleController {
+
+    @GetMapping(value = "/welcome", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> welcomeMessage() {
+        return new ResponseEntity<String>("Hello world from controller!!!", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/welcome", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public String welcomeMessageJson() {
+        return "Hello world from controller json!!!";
+    }
+
+    @GetMapping(value = "/welcome", produces = MediaType.APPLICATION_XML_VALUE)
+    public String welcomeMessageXml() {
+        return "Hello world from controller xml!!!";
+    }
+
+
+}
---
+++ src/main/java/com/fujitsu/serverview/ui/SimpleControllerTest.java
@@ -0,0 +1,15 @@
+package com.fujitsu.serverview.ui;
+
+
+import org.junit.Test;
+
+/**
+ * @author Maciej Solinski <a href="mailto:maciej.solinski@ts.fujitsu.com">maciej.solinski@ts.fujitsu.com</a>
+ */
+public class SimpleControllerTest {
+
+    @Test
+    public void welcomeMessage() throws Exception {
+    }
+
+}
